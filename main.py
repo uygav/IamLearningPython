42dba1a2372aec4f8cf2fb3b9ba866a5da48cad2
Uygar Muntaş <muntasuygar@gmail.com>
2023-05-03 19:17:36 +0300

    threading

--- main.py
+++ main.py
@@ -1086,5 +1086,42 @@ import time
 
 ################################################## threading
 
+import threading
+import time
+
+print(threading.active_count())
+print(threading.enumerate())
+print(time.perf_counter())
+def eat_breakfast():
+    time.sleep(3)
+    print("you eat breakfast")
+def drink_coffee():
+    time.sleep(4)
+    print("you drank coffee")
+def study():
+    time.sleep(5)
+    print("you finish studying")
+
+# eat_breakfast()
+# drink_coffee()
+# study()
+
+##############################
+
+x = threading.Thread(target=eat_breakfast,args=())
+x.start()
+
+y = threading.Thread(target=drink_coffee,args=())
+y.start()
+
+z = threading.Thread(target=study,args=())
+z.start()
+
+x.join()
+y.join()
+z.join()
+
+
+
 
 
