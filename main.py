45bd6e497d728cd8b471843265a98619de2bf994
Uygar Muntaş <muntasuygar@gmail.com>
2023-05-12 23:46:26 +0300

    frames

--- main.py
+++ main.py
@@ -1601,45 +1601,60 @@ from tkinter import *
 
 ################################################## menubar
 
+# from tkinter import *
+#
+# def openFile():
+#     print("File has been opened")
+#
+# def saveFile():
+#     print("File has ben saved")
+#
+# def cut():
+#     print("you cut some text")
+#
+# def copy():
+#     print("you copied some text")
+#
+# def paste():
+#     print("you pasted some text")
+#
+# window = Tk()
+#
+# menubar = Menu(window)
+# window.config(menu=menubar)
+#
+# fileMenu = Menu(menubar,tearoff=0,font=("MV Boli",15))
+# menubar.add_cascade(label="File",menu=fileMenu)
+# fileMenu.add_command(label="Open",command=openFile)
+# fileMenu.add_command(label="Save",command=saveFile)
+# fileMenu.add_separator()
+# fileMenu.add_command(label="Exit",command=quit)
+#
+# editMenu = Menu(menubar,tearoff=0,font=("MV Boli",15))
+# menubar.add_cascade(label="Edit",menu=editMenu)
+# editMenu.add_command(label="Cut",command=cut)
+# editMenu.add_command(label="Copy",command=copy)
+# editMenu.add_command(label="Paste",command=paste)
+# ### you can also add some images
+#
+# window.mainloop()
+
+
+################################################## frames
+
 from tkinter import *
 
-def openFile():
-    print("File has been opened")
+window =Tk()
 
-def saveFile():
-    print("File has ben saved")
+frame = Frame(window,bg="pink",bd=5,relief=SUNKEN)
+frame.place(x=0,y=0)
 
-def cut():
-    print("you cut some text")
-
-def copy():
-    print("you copied some text")
-
-def paste():
-    print("you pasted some text")
-
-window = Tk()
-
-menubar = Menu(window)
-window.config(menu=menubar)
-
-fileMenu = Menu(menubar,tearoff=0,font=("MV Boli",15))
-menubar.add_cascade(label="File",menu=fileMenu)
-fileMenu.add_command(label="Open",command=openFile)
-fileMenu.add_command(label="Save",command=saveFile)
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit",command=quit)
-
-editMenu = Menu(menubar,tearoff=0,font=("MV Boli",15))
-menubar.add_cascade(label="Edit",menu=editMenu)
-editMenu.add_command(label="Cut",command=cut)
-editMenu.add_command(label="Copy",command=copy)
-editMenu.add_command(label="Paste",command=paste)
-### you can also add some images
+Button(frame,text="W",font=("Consolas",25),width=3).pack(side=TOP)
+Button(frame,text="A",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(frame,text="S",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(frame,text="D",font=("Consolas",25),width=3).pack(side=LEFT)
 
 window.mainloop()
-
-
 
 
 
